32b210df983fe87ed6c9fcf4a96419c36459e38b
Rohini <rohini_subramanian@outlook.com>
2021-11-25 22:58:50 +0800

    Initialise/Register the filter chain

---
+++ src/main/java/springSecurity/SecurityInitialiser.java
@@ -0,0 +1,11 @@
+package springSecurity;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/*
+This class will look for the filter chain that has been created
+and register the filter with the application
+ */
+public class SecurityInitialiser extends AbstractSecurityWebApplicationInitializer {
+
+}
